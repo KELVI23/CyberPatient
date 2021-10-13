5ce4d524052f3ec57838764f0a118b2f4300953e
Kelvin Musodza <kelvinledoit@gamil.com>
2021-10-12 18:48:05 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+
+    //fecth data
+    $.getJSON("medications.json", function (data){
+        var jsonData = '';
+        var filter = '';
+
+        //iterating through objects
+        $.each(data, function (key, value) {
+            
+            //contruction of rows 
+            jsonData += '<tr>';
+            jsonData += '<td>' + value.category + '</td>';
+            
+            jsonData += '<td>' + value.name + '</td>';
+
+            jsonData += '<td>' + value.dose + '</td>';
+
+            jsonData += '<td>' + value.description + '</td>';
+
+            jsonData += '<td>' + value.cost + '</td>';
+
+            jsonData += '</tr>';
+
+        });
+
+        //inserting rows into table
+      $('#table').append(jsonData);
+           
+    });
+});
+
+//filter table by category
+function filter(){
+    var categoryFilter = (data)=>{
+        return data.category == "ALIMENTARY TRACT AND METABOLISM";
+    }
+    /** Do nothing error with function */
+ //   $('#table').tabulator("setFilter", categoryFilter);
+ 
+}
+
+function category() {
+document.getElementById('filtered').style.visibility = "visible";
+}
+
+//remove table filter
+function clearFilter(){
+    document.getElementById('filtered').style.visibility = "hidden"; 
+   // $("#table").tabulator("clearFilter");
+   
+}
+
+function searchFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through  table rows, and hide rows that do not match the search
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+
